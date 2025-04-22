a1334d6931f0414fea8bdc1e534010f9b157b47f
RupendraCHA <chandaluri210@gmail.com>
2025-04-22 06:41:26 +0530

    Ebedding Power BI Dashboards within React JS

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PowerBIReport from "./PowerBIReport";
+
+function App() {
+  return (
+    <div className="App">
+      <PowerBIReport />
+    </div>
+  );
+}
+
+export default App;
